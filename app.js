a74fbd9c99457f99c3c1cf040e8ad9f4745c02c1
Graeme Foster <graemef@gmail.com>
2014-06-18 12:17:39 +0100

    Display events from the audit collection.

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.get('/', routes.index);
 app.get('/timeline', timeline.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Listening on port ' + app.get('port'));
 });
--- routes/timeline.js
+++ routes/timeline.js
@@ -1,5 +1,19 @@
 var MongoClient = require('mongodb').MongoClient;
 var _ = require('lodash');
+var moment = require('moment');
+
+var createVisDataItem = function (item) {
+  var start = moment(item.CreatedDateTime)
+      .subtract(item.Event.ElapsedMilliseconds, 'milliseconds');
+
+  return {
+    id: item._id,
+    group: item.Context.RequestId,
+    content: item.Event._t,
+    start: start,
+    end: item.CreatedDateTime
+  };
+};
 
 exports.list = function (req, res) {
 
@@ -9,17 +23,18 @@ exports.list = function (req, res) {
     }
 
     var collection = db.collection(process.env.AUDIT_MONGODB_COLLECTION);
-    collection
+    return collection
         .find({'Event.ElapsedMilliseconds': {$exists: true}})
         .toArray(function (err, items) {
           if (err) {
             return res.send(500, err);
           }
 
-          return res.json(_(items).chain()
-              .map(function (item) {
-                return item;//item.Event.hasOwnProperty('ElapsedMilliseconds');
-              }).valueOf());
+          return res
+              .json(_(items)
+                  .chain()
+                  .map(createVisDataItem)
+                  .valueOf());
         });
   });
 
