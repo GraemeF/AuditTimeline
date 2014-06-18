448e54c57761a5bf3bd5fc4eec4603c2db80fc98
Graeme Foster <graemef@gmail.com>
2014-06-18 11:17:03 +0100

    Respond with everything.

--- routes/timeline.js
+++ routes/timeline.js
@@ -1,3 +1,20 @@
-exports.list = function(req, res){
-  res.send("respond with a resource");
+var MongoClient = require('mongodb').MongoClient;
+
+exports.list = function (req, res) {
+
+  MongoClient.connect(process.env.AUDIT_MONGODB_CONNECTIONSTRING, function (err, db) {
+    if (err) {
+      return res.send(500, err);
+    }
+
+    var collection = db.collection(process.env.AUDIT_MONGODB_COLLECTION);
+    collection.find().toArray(function (err, items) {
+      if (err) {
+        return res.send(500, err);
+      }
+
+      return res.json(items);
+    });
+  });
+
 };
