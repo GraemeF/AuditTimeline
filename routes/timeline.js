0b8afbff2b51f8e7a4149620c90d0f6ec4a1adeb
Graeme Foster <graemef@gmail.com>
2014-06-18 11:41:29 +0100

    Find events with elapsed milliseconds only.

--- routes/timeline.js
+++ routes/timeline.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
+var _ = require('lodash');
 
 exports.list = function (req, res) {
 
@@ -8,13 +9,18 @@ exports.list = function (req, res) {
     }
 
     var collection = db.collection(process.env.AUDIT_MONGODB_COLLECTION);
-    collection.find().toArray(function (err, items) {
-      if (err) {
-        return res.send(500, err);
-      }
+    collection
+        .find({'Event.ElapsedMilliseconds': {$exists: true}})
+        .toArray(function (err, items) {
+          if (err) {
+            return res.send(500, err);
+          }
 
-      return res.json(items);
-    });
+          return res.json(_(items).chain()
+              .map(function (item) {
+                return item;//item.Event.hasOwnProperty('ElapsedMilliseconds');
+              }).valueOf());
+        });
   });
 
 };
